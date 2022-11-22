140562a1cc55be45bb0ab428980bd49b0f9513d9
rafaelalma <rafaelalma92@gmail.com>
2022-11-22 21:36:28 +0100

    Clean storybook files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,5 @@
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/components/**/*.stories.tsx",
-  ],
+  stories: ["../src/components/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",

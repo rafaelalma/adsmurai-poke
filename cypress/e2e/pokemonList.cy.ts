07a3b4456f0ea8395e3eddbd23b6aee5e727d038
rafaelalma <rafaelalma92@gmail.com>
2022-12-06 18:22:24 +0100

    Write pokemon list e2e test

--- cypress/e2e/pokemonList.cy.ts
+++ cypress/e2e/pokemonList.cy.ts
@@ -2,9 +2,12 @@ describe("Pokemon list", () => {
   it("Shows a list with Pokemon names", () => {
     // GIVEN
     // The user is in the home page
+    cy.visit("http://localhost:3000");
     // WHEN
     // The user clicks in the Pokedex link
+    cy.contains("Pokedex").click();
     // THEN
     // The app navigates to the Pokemon list page and shows a list with Pokemon names
+    cy.get("h1").should("contain", "Pokemon's List");
   });
 });

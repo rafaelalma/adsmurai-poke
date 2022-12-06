d261be3076743a3b75f9a7c3dc1434e9279fb10f
rafaelalma <rafaelalma92@gmail.com>
2022-12-06 21:52:41 +0100

    Add pokemon list types

---
+++ src/types/pokemonType.ts
@@ -0,0 +1,11 @@
+export type PokemonListElement = {
+  name: string;
+  url: string;
+};
+
+export type PokemonListResponse = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PokemonListElement[];
+};

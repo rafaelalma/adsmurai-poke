065490f196eb9701682a4f13395cb8f7d7893485
rafaelalma <rafaelalma92@gmail.com>
2022-12-08 15:31:20 +0100

    Add home to pokedex navigation test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,25 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { RouterProvider } from "react-router-dom";
 
 import routerInstance from "routerInstance";
 
-test("Renders App", () => {
-  render(<RouterProvider router={routerInstance} />);
+describe("App", () => {
+  it("renders", () => {
+    render(<RouterProvider router={routerInstance} />);
 
-  const homeTitle = screen.getByText("Home");
-  expect(homeTitle).toBeInTheDocument();
+    const homeTitle = screen.getByRole("heading", { name: "Home" });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  it("navigates from home page to pokedex page", async () => {
+    render(<RouterProvider router={routerInstance} />);
+
+    userEvent.click(screen.getByRole("link", { name: "Pokedex" }));
+
+    const pokedexTitle = await screen.findByRole("heading", {
+      name: "Pokedex",
+    });
+    expect(pokedexTitle).toBeInTheDocument();
+  });
 });

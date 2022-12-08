d2cf0ab9b61975eb35b29c617c69147df0c36043
rafaelalma <rafaelalma92@gmail.com>
2022-12-08 20:56:28 +0100

    Write pokemon list page navigation e2e test

--- cypress/e2e/pokemonListPageNavigation.cy.ts
+++ cypress/e2e/pokemonListPageNavigation.cy.ts
@@ -2,14 +2,21 @@ describe("Pokemon list page navigation", () => {
   it("There are next and previous buttons to allow navigation between pages in the Pokemon list", () => {
     // GIVEN
     // The user is in the Pokedex page
+    cy.visit("/pokedex");
     // WHEN
     // The user clicks the Next button
+    cy.contains("button", "Next").click();
     // THEN
     // The app navigates to the next page in the Pokemon list
+    cy.url().should("include", "?offset=20");
+    cy.get("ul").get("li").first().should("contain", "Spearow");
     // AND
     // WHEN
     // The user clicks the Previous button
+    cy.contains("button", "Previous").click();
     // THEN
     // The app navigates to the previous page in the Pokemon list
+    cy.url().should("include", "?offset=0");
+    cy.get("ul").get("li").last().should("contain", "Raticate");
   });
 });

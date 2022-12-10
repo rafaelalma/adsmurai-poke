e96f309ef62a0143c2d3f648acc05ea07c9f2588
rafaelalma <rafaelalma92@gmail.com>
2022-12-10 21:05:27 +0100

    Change ErrorPage styles

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,19 +1,21 @@
 import { ReactElement } from "react";
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage(): ReactElement {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div>
-      <h1 className="title">Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      {isRouteErrorResponse(error) ? (
-        <p>
-          <i>{error.statusText || error.error?.message}</i>
-        </p>
-      ) : null}
+    <div className="app-wrapper">
+      <div className="page-wrapper page-wrapper--error">
+        <h1 className="title">Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        {error instanceof Error ? (
+          <p>
+            <strong>{error.message}</strong>
+          </p>
+        ) : null}
+      </div>
     </div>
   );
 }

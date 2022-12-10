626e36622e594be55f2a6b3902166d7067782815
rafaelalma <rafaelalma92@gmail.com>
2022-12-10 14:54:32 +0100

    Remove redundant navigation test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { RouterProvider } from "react-router-dom";
 
 import routerInstance from "routerInstance";
@@ -10,16 +9,5 @@ describe("App", () => {
 
     const homeTitle = screen.getByRole("heading", { name: "Home" });
     expect(homeTitle).toBeInTheDocument();
-  });
-
-  it("navigates from home page to pokedex page", async () => {
-    render(<RouterProvider router={routerInstance} />);
-
-    userEvent.click(screen.getByRole("link", { name: "Pokedex" }));
-
-    const pokedexTitle = await screen.findByRole("heading", {
-      name: "Pokedex",
-    });
-    expect(pokedexTitle).toBeInTheDocument();
   });
 });

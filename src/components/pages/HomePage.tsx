bb5716b3d197708d19f04f16159e1b47ee37a242
rafaelalma <rafaelalma92@gmail.com>
2022-12-11 16:04:59 +0100

    Add pokeball image to home page

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,9 +1,22 @@
 import { ReactElement } from "react";
+import pokeball from "images/pokeball.png";
+
+import "./HomePage.scss";
 
 export default function HomePage(): ReactElement {
   return (
-    <div>
+    <div className="home-page-wrapper">
       <h1 className="title">Home</h1>
+      <img className="home-image" src={pokeball} alt="pokeball" />
+      <a
+        className="home-link"
+        href="https://www.flaticon.com/free-icons/pokemon"
+        title="pokemon icons"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Pokemon icons created by Nikita Golubev - Flaticon
+      </a>
     </div>
   );
 }
